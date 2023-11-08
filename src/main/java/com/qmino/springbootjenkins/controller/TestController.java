7e7aa19ce5eba5cfa91549e83e0722d016a4cde3
Dimitri Caluwaerts <dimitri.caluwaerts@qmino.com>
2023-11-08 14:47:06 +0100

    test

--- src/main/java/com/qmino/springbootjenkins/controller/TestController.java
+++ src/main/java/com/qmino/springbootjenkins/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.qmino.springbootjenkins.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping(name = "/test")
-    public String test() {
-        return "test!";
+    @GetMapping(value = "/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.status(200).body("test!");
     }
 }
